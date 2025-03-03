7a4fdde20f6409564c77d8528b3b205ba8f45182
Winga78 <lab.winga@gmail.com>
2025-03-03 10:46:10 +0100

    initalisation du projet

--- frontend/src/test/backendConnection.test.ts
+++ frontend/src/test/backendConnection.test.ts
@@ -2,14 +2,14 @@ import { describe, it, expect } from "vitest";
 
 describe("Auth-service Connection", () => {
   it("doit répondre avec un statut 200", async () => {
-    const response = await fetch("http://localhost:3000/");
+    const response = await fetch(process.env.VITE_AUTH_SERVICE_URL || "http://auth-service:3000");
     expect(response.status).toBe(200);
   });
 });
 
 describe("Chat-service Connection", () => {
     it("doit répondre avec un statut 200", async () => {
-      const response = await fetch("http://localhost:3001/");
+      const response = await fetch(process.env.VITE_VOTE_SERVICE_URL||"http://localhost:3001/");
       expect(response.status).toBe(200);
     });
 });
@@ -17,7 +17,7 @@ describe("Chat-service Connection", () => {
 
 describe("Vote-service Connection", () => {
     it("doit répondre avec un statut 200", async () => {
-      const response = await fetch("http://localhost:3003/");
+      const response = await fetch(process.env.VITE_CHAT_SERVICE_URL||"http://vote-service:3003");
       expect(response.status).toBe(200);
     });
 });
