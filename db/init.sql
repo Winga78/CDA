6e9fbc023c1c3addb02b5b38d852c08e308bb67e
Winga78 <lab.winga@gmail.com>
2025-03-20 14:35:13 +0100

    test

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS dev_cda_chat;
+CREATE DATABASE IF NOT EXISTS dev_cda_project;
+CREATE USER IF NOT EXISTS 'wiwi'@'%' IDENTIFIED BY 'wiwi123';
+GRANT ALL PRIVILEGES ON dev_cda_chat.* TO 'wiwi'@'%';
+GRANT ALL PRIVILEGES ON dev_cda_project.* TO 'wiwi'@'%';
+FLUSH PRIVILEGES;
