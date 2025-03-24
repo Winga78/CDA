fa577b4026e8b94cb5e15bd41a1647969acadd12
Winga78 <lab.winga@gmail.com>
2025-03-24 23:53:33 +0100

    autre idéee

---
+++ services/project-user-post-service/test/constantsPost.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProjectUserModule } from "../src/project-user/project-user.module";
+import { PostUserModule } from '../src/post-user/post-user.module';
+import { AppModule } from '../src/app.module';
+
+export const database  = {
+    type :(process.env.DB_TYPE as any)|| "mysql",
+    host : process.env.DB_HOST || "host",
+    port : process.env.DB_PORT || "3306",
+    username : process.env.MYSQL_USER || "db_user",
+    password : process.env.MYSQL_PASSWORD || "db_password",
+    database : process.env.DB_DATABASE_RELATION || "dev_cda_project_user_post",
+}
+
+export const imports = [
+    TypeOrmModule.forRoot(database),
+    AppModule,
+    PostUserModule
+];
--- services/project-user-post-service/test/postUser.e2e-spec.ts
+++ services/project-user-post-service/test/postUser.e2e-spec.ts
@@ -2,7 +2,7 @@ import { INestApplication, HttpStatus } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
 import { DataSource } from 'typeorm';
-import { database, imports } from './constants';
+import { database, imports } from './constantsPost';
 import { faker } from '@faker-js/faker';
 import { PostUser } from '../src/post-user/entities/post-user.entity';
 import axios from 'axios';
