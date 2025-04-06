da616f99807ac2269d94879728943e83b48a9c80
Winga78 <lab.winga@gmail.com>
2025-04-06 23:58:26 +0200

    mise à jour

--- services/chat-service/src/main.ts
+++ services/chat-service/src/main.ts
@@ -2,11 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 dotenv.config(); 
-import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useWebSocketAdapter(new IoAdapter(app));
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
