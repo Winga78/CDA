f297778fa3b00731cbcf62be614b4976efda9ba1
Winga78 <lab.winga@gmail.com>
2025-04-10 13:02:09 +0200

    test

--- services/project-user-post-service/src/config/database.config.ts
+++ services/project-user-post-service/src/config/database.config.ts
@@ -5,7 +5,5 @@ export const CONFIG_DATABASE = 'database';
 export default registerAs(CONFIG_DATABASE, () => ({
   users: {
     uri: process.env.MONGODB_URI,
-  },
-  api_project_URL : process.env.PROJECT_DOCKER_URL||"http://localhost:3002",
-  api_auth_URL : process.env.AUTH_DOCKER_URL|| "http://localhost:3000"
+  }
 }));
