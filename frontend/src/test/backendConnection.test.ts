fb7b6b8eab1afa46686cef1eaf273ea6151aa6f6
Winga78 <lab.winga@gmail.com>
2025-04-10 12:51:25 +0200

    tessst

--- frontend/src/test/backendConnection.test.ts
+++ frontend/src/test/backendConnection.test.ts
@@ -2,14 +2,14 @@ import { describe, it, expect } from "vitest";
 //test
 describe("Auth-service Connection", () => {
   it("doit répondre avec un statut 200", async () => {
-    const response = await fetch(process.env.VITE_AUTH_SERVICE_URL || "api/auth");
+    const response = await fetch(process.env.VITE_AUTH_SERVICE_URL || "http://localhost:3000");
     expect(response.status).toBe(200);
   });
 });
 
 describe("Chat-service Connection", () => {
     it("doit répondre avec un statut 200", async () => {
-      const response = await fetch(process.env.VITE_CHAT_SERVICE_URL ||"api/chat");
+      const response = await fetch(process.env.VITE_CHAT_SERVICE_URL ||"http://localhost:3001");
       expect(response.status).toBe(200);
     });
 });
@@ -17,7 +17,7 @@ describe("Chat-service Connection", () => {
 
 describe("Project-service Connection", () => {
     it("doit répondre avec un statut 200", async () => {
-      const response = await fetch(process.env.VITE_PROJECT_SERVICE_URL || "api/projects") ;
+      const response = await fetch(process.env.VITE_PROJECT_SERVICE_URL || "http://localhost:3002") ;
       expect(response.status).toBe(200);
     });
 });
@@ -25,7 +25,7 @@ describe("Project-service Connection", () => {
 
 describe("project_user_post-service Connection", () => {
   it("doit répondre avec un statut 200", async () => {
-    const response = await fetch(process.env.VITE_PROJECT_USER_POST_SERVICE_URL||"api/project-user-post");
+    const response = await fetch(process.env.VITE_PROJECT_USER_POST_SERVICE_URL||"http://localhost:3003");
     expect(response.status).toBe(200);
   });
 });
