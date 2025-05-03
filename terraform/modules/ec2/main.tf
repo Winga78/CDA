2218051c8110a891e79c3fa39cb54f69be671334
Winga78 <lab.winga@gmail.com>
2025-05-03 19:25:09 +0200

    correction

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -16,10 +16,15 @@ chmod 600 /home/ubuntu/.ssh/authorized_keys
 EOF
 
   tags = {
-    Name = "JacksBlogExample"
+    Name = "CDAInstance"
   }
 }
-resource "aws_eip" "cda_ip" {
-  instance = aws_instance.cda_server.id
-  vpc      = true
+
+resource "aws_eip" "this" {
+  domain = "vpc"
+}
+
+resource "aws_eip_association" "example" {
+  instance_id   = aws_instance.cda_server.id
+  allocation_id = aws_eip.this.id
 }
