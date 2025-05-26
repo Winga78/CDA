530631b8fdc0fd1767c438c9ec870141b89900d2
Winga78 <lab.winga@gmail.com>
2025-05-27 00:20:23 +0200

    prefixage

--- services/chat-service/src/main.ts
+++ services/chat-service/src/main.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/posts');
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
--- services/project-user-post-service/src/main.ts
+++ services/project-user-post-service/src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/project-user');
+  app.setGlobalPrefix('api/post-user');
   await app.listen(process.env.PORT ?? 3003);
 }
 bootstrap();
