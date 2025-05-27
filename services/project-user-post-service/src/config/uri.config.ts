f5a1c752e3b9179d971f7c036b62920baea68fe7
Winga78 <lab.winga@gmail.com>
2025-05-27 21:37:55 +0200

    hjhjhj

--- services/project-user-post-service/src/config/uri.config.ts
+++ services/project-user-post-service/src/config/uri.config.ts
@@ -1,11 +1,14 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); 
+
 const isProd = process.env.NODE_ENV === 'production';
 
 export const SERVICE_URLS = {
   user: isProd
-    ? `${process.env.USER_SERVICE_BASE_URL}/users`
-    : `${process.env.USER_SERVICE_BASE_URL}`,
+    ? `${process.env.USER_SERVICE_BASE_URL}`
+    : `${process.env.USER_SERVICE_BASE_URL}/users`,
 
   project: isProd
-    ? `${process.env.PROJECT_SERVICE_BASE_URL}/projects`
-    : `${process.env.PROJECT_SERVICE_BASE_URL}`,
+    ? `${process.env.PROJECT_SERVICE_BASE_URL}`
+    : `${process.env.PROJECT_SERVICE_BASE_URL}/projects`,
 };
